770f1d41ba314ec9920d7fae0315b072e0df0ba9
FelipePDS <felipepdasilva66@gmail.com>
2021-06-13 21:30:10 -0300

    adding play from PlayerContext

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,9 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
+
 import styles from './home.module.scss';
 
 type Episode = {
@@ -26,6 +29,8 @@ type HomeProps = {
 };
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
+
   return (
     <div className={styles.homepage}>
         <section className={styles.latestEpisodes}>
@@ -52,7 +57,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                                 <span>{episode.durationAsString}</span>
                             </div>
 
-                            <button type="button">
+                            <button type="button" onClick={() => play(episode)}>
                                 <img src="/play-green.svg" alt="Tocar episódio" />
                             </button>
                         </li>
