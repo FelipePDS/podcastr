a6edecff7dec27afe093d0911cdd4226d32bea28
FelipePDS <felipepdasilva66@gmail.com>
2021-06-14 21:59:14 -0300

    refactoring PlayerContext implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,39 +2,13 @@ import '../styles/global.scss';
 
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
-import { PlayerContext } from '../contexts/PlayerContext';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
 
 import styles from '../styles/app.module.scss';
-import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  };
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
-  };
-
-  function setPlayingState(state: boolean) {
-    setIsPlaying(state);
-  };
-
   return (
-      <PlayerContext.Provider value={{ 
-        episodeList, 
-        currentEpisodeIndex, 
-        isPlaying, 
-        play, 
-        togglePlay, 
-        setPlayingState 
-      }}>
+      <PlayerContextProvider>
           <div className={styles.wrapper}>
             <main>
                 <Header />
@@ -43,7 +17,7 @@ function MyApp({ Component, pageProps }) {
             
             <Player />
         </div>
-      </PlayerContext.Provider>
+      </PlayerContextProvider>
   )
 }
 
