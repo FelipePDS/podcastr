546fefc152accbc1a6a494142b3385270a9cdb5f
FelipePDS <felipepdasilva66@gmail.com>
2021-06-14 21:58:44 -0300

    remove page global title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,9 +8,7 @@ export default class extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet" />
 
-                    <link rel="icon" href="/favicon.png" />
-
-                    <title>Podcastr</title>
+                    <link rel="shortcut icon" href="/favicon.png" type="image/png" />
                 </Head>
 
                 <body>
